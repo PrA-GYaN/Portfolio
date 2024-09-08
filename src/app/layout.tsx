52a4c4174393f99a50fa7aceeeb2439b9de9b409
PrA-GYaN <pragyan1516@gmail.com>
2024-09-08 10:56:42 +0545

    Used SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Inter } from 'next/font/google'
 
 // Initialize the Inter font
